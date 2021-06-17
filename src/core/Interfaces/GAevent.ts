829b1a3d308952def576ac0412b583bacb4a1e3b
DawidRubch <dawid.niegrebecki@gmail.com>
2021-06-17 18:59:46 +0200

    Implemented types for ReactGA

---
+++ src/core/Interfaces/GAevent.ts
@@ -0,0 +1,22 @@
+export enum Category {
+  PARAMETER_CHOICE = "ParameterChoice",
+  RESERVATION = "Reservation",
+}
+
+//Action used mostly for parameter type
+export enum Action {
+  SAVED_RESERVATION = "SavedReservation",
+  PEOPLE = "People",
+  TIME = "Time",
+  DATE = "Date",
+}
+
+//Label value in React-ga event, will be used as a page name
+export enum Label {
+  MAIN = "Main",
+  ARRAY = "Array",
+  DESCRIPTION = "Description",
+  CONFIRM = "Confirm",
+}
+
+export interface GAevent {}
