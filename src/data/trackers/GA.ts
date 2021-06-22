52cbfb816ec7850dd3d5cc4722c7dea40e13f9e4
DawidRubch <dawid.niegrebecki@gmail.com>
2021-06-23 01:07:55 +0200

    Added ga4React object

--- src/data/trackers/GA.ts
+++ src/data/trackers/GA.ts
@@ -1,8 +1,10 @@
 import { Category, GAevent } from "../../core/Interfaces/GAevent";
 
+import GA4React from "ga-4-react";
 
 //Class responsible for Google analitics tracking
 class GA {
+  ga4React = new GA4React("G-SN0VJY0RPS");
   trackEvent = ({ category, label, action }: GAevent) => {
     window.gtag("event", action, {
       event_category: category,
