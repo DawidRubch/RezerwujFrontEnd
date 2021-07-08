44eda35e56dd2658aab729db2b00416aa68eb0f4
DawidRubch <dawid.niegrebecki@gmail.com>
2021-07-08 19:23:15 +0200

    Changed env variable name

--- src/data/database/RestaurantOrPubRemoteDb.ts
+++ src/data/database/RestaurantOrPubRemoteDb.ts
@@ -15,7 +15,7 @@ export class RestaurantOrPubRemoteDb {
       "Access-Control-Allow-Origin": "*",
     },
   };
-  enviromentType = process.env.ENVIROMENT_VARIABLE;
+  enviromentType = process.env.NODE_ENV;
 
   async getRestaurantsFromDb(bookTime: BookTime, address?: string) {
     let bookTimeToJson = bookTime.toJson();
